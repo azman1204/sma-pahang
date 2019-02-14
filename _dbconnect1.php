e8c6e6ad02a82259e24d6b47781d2d07c9c2747d
Azman Zakaria <azman120474@gmail.com>
2019-02-14 15:43:57 +0800

    Create _dbconnect1.php

---
+++ _dbconnect1.php
@@ -0,0 +1,13 @@
+foreach ($_POST as $key => $val) {
+    // kes array dalam array
+    if(is_array($_POST[$key])) {
+        echo $_POST[$key] . '= array';
+        $arr = [];
+        foreach($val as $val2) {
+            $arr[] = mysqli_real_escape_string($connx, $val2);
+        }
+        $_POST[$key] = $arr;
+    } else {
+        $_POST[$key] = mysqli_real_escape_string($connx, $val);
+    }
+}
